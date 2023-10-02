281ad56477164ffa4bd943b607d4baf52b5708c7
Milagros <mvcruz@fi.uba.ar>
2023-10-02 09:19:51 -0300

    path validation added

--- cli/cli.py
+++ cli/cli.py
@@ -2,15 +2,23 @@ import os
 import sys
 import subprocess
 
+PYTHON_VERSION = "python3"
 CMD_DIR = "cli/cmd"
 
+# RunCommand takes a command name, finds its path and executes it
+# If no path is found, it prints a message error
 def RunCommand(cmd):
     os.chdir("..")
-    subprocess.call(["python3", "{}/{}/{}.py".format(os.getcwd(), CMD_DIR, cmd)])
+    script_path =  "{}/{}/{}.py".format(os.getcwd(), CMD_DIR, cmd)
+    if os.path.exists(script_path):
+        print("Executing command ", cmd)
+        subprocess.call([PYTHON_VERSION, script_path])
+    else:
+        print("Invalid command ", cmd)
 
+# This loop iterates over the command line arguments received and runs them 
 for i in range(1, len(sys.argv)):
     cmd = sys.argv[1]
-    print( "-----------------------------------------")
-    print("Executing command ", cmd)
+    print( "\n-----------------------------------------")
 
     RunCommand(cmd)
