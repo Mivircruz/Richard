136d2b0beb4239b81b961a35f021eac55c90252a
Milagros <33661184+Mivircruz@users.noreply.github.com>
2023-12-31 20:03:11 -0300

    Premake bin location changed + Richard engine added to premake + Capital letters used in editor and engine name (#3)
    - Pemake bin location changed 
    
    - Richard engine added to premake

--- richardeditor/src/main.cpp
+++ richardeditor/src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "richard/engine.h"
+
 using namespace std;
 
 int main(void)
 {
     cout << "Hello World! I'm Richard :)" << endl;
- 
+    int a = Richard::Add(1,7);
+    cout << "Add(1,7) result: " << a << endl;
     return 0;
 }
