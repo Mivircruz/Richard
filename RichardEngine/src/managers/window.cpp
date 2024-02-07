1a4441cf4dd3c217e6989a9791079ccf9cc5f456
Milagros <33661184+Mivircruz@users.noreply.github.com>
2024-02-07 09:50:12 -0300

    WINDOW - GLAD Setup
    * GLAD lib added + premake GLAD created + GLAD added to Richard's premake
    
    * OpenGL added to Window ManagerL

--- RichardEngine/src/managers/window.cpp
+++ RichardEngine/src/managers/window.cpp
@@ -2,6 +2,7 @@
 #include "constants.h"
 #include "tools/logger.h"
 #include "SDL.h"
+#include "glad/glad.h"
 
 #include <iostream>
 
@@ -11,27 +12,56 @@ namespace Richard::Managers {
 	/*Public methods*/
 	
 	Window::Window() {
-		window = nullptr;
+		mWindow = nullptr;
 	} 
 
 	Window::~Window() {
-		if (window) {
+		if (mWindow) {
 			Shutdown();
 		}
 	}
 
 	int Window::Initialize() {
-		window = SDL_CreateWindow("RichardGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-		if (!window) {
+		// Before the window creation, we specify the OpenGL version and the profile that we want
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, GLAD_MAJOR_VERSION);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, GLAD_MINOR_VERSION);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+		// Graphic specifications: 
+		// Window updates twice as often and has 24 bits for the color
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, GLAD_DEPTH_SIZE);
+
+		// Window creation for our platform
+		mWindow = SDL_CreateWindow("RichardGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_WIDTH, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+		if (!mWindow) {
 			Tools::Logger::Error("Error creating window: " + string(SDL_GetError()));
-			return W_INTIALIZE_FAIL;
+			return W_INTIALIZE_SDL_WINDOW_FAIL;
 		}
+	
+		// Set the minimum size of a window's client area.
+		SDL_SetWindowMinimumSize(mWindow, WINDOW_MIN_WIDTH, WINDOW_MIN_HEIGHT);
+
+		// OpenGL Context creation
+		// It sets up the graphics context
+		mGLContext = SDL_GL_CreateContext(mWindow);
+		if(!mGLContext) {
+			Tools::Logger::Error("Error creating OpenGL Context: " + string(SDL_GetError()));
+			return W_INITIALIZE_OPENGL_CONTEXT_FAIL;
+		}
+
+		// Set up the funcion pointers
+		gladLoadGLLoader(SDL_GL_GetProcAddress);
+
+		// Set the background color
+		glClearColor(0.0,0.0,1.0,0.0);
+
 		return W_INTIALIZE_OK;
 	}
 
 	void Window::Shutdown() {
-		SDL_DestroyWindow(window);
-		window = nullptr;
+		SDL_DestroyWindow(mWindow);
+		mWindow = nullptr;
 	}
 
 	int Window::HandleEvents() {
@@ -45,7 +75,15 @@ namespace Richard::Managers {
 				break;
 			}
 		}
-
 		return EVENT_DEFAULT;
+	}
+
+	void Window::Render() {
+		// Clears the color of the screen (color buffer) and
+		// any cached information about the depth of what the window just rendered (depth buffer)
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Updates the window with OpenGL rendering.
+		SDL_GL_SwapWindow(mWindow);
 	}
 }
