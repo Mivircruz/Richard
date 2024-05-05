e25653516c79339342dd67eef5c4545d15e1a44d
Milagros <mvcruz@fi.uba.ar>
2024-05-05 01:10:36 -0300

    old shader string removed

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -17,30 +17,6 @@ class ClientApp : public Richard::Application {
     void Initialize() override {
         std::cout << "ClientApp Initialize" << std::endl;
 
-        //Define the shaders
-        const char *vertexShader = "#version 410 core\n"
-            "layout (location = 0) in vec3 aPos;\n"
-            "layout (location = 1) in vec3 aColor;\n"
-            "layout(location = 2) in vec2 aTexCoord;\n"
-            "out vec3 ourColor;\n"
-            "out vec2 TexCoord;\n"
-            "void main()\n"
-            "{\n"
-            "   gl_Position = vec4(aPos, 1.0);\n"
-            "   ourColor = aColor;\n"
-            "   TexCoord = aTexCoord;\n"
-            "}\0";
-
-        const char* fragmentShader = "#version 410 core\n"
-            "out vec4 FragColor;\n"
-            "in vec3 ourColor;\n"
-            "in vec2 TexCoord;\n"
-            "uniform sampler2D ourTexture;\n"
-            "void main()\n"
-            "{\n"
-            "  FragColor = texture(ourTexture, TexCoord);\n"
-            "}\n\0";
-
         mShader = make_shared<Richard::Graphics::Shader>("resources/shaders/texture_vs.txt", "resources/shaders/texture_fs.txt");
 
         // Define the square
